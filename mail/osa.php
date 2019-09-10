04ed468b522eaf7d5e2cb58689bde8d050a78f1f
Erika <>
2019-09-10 14:22:55 +0200

    no message

---
+++ mail/osa.php
@@ -0,0 +1,24 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+header('Content-Type: application/json');
+if ($name === ''){
+  print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
+  exit();
+}
+if ($email === ''){
+  print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
+  exit();
+} else {
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+  print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
+  exit();
+  }
+}
+$content="From: $name \nEmail: $email";
+$recipient = "erika.vastberg@gmail.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+print json_encode(array('message' => 'Email successfully sent!', 'code' => 1));
+exit();
+?>
